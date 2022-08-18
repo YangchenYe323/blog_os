ade89b159a241f56edaacbc9fb67bce68f214486
Ye Yangchen <eikasia30@gmail.com>
2022-08-18 17:15:22 -0500

    Add a real heap allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,6 +2,13 @@
 
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
+use linked_list_allocator::LockedHeap;
+use x86_64::{
+  structures::paging::{
+    mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
+  },
+  VirtAddr,
+};
 
 /// A dummy allocator implementation that would return nullptr
 /// for every call to alloc
@@ -18,10 +25,53 @@ unsafe impl GlobalAlloc for Dummy {
   }
 }
 
-#[global_allocator]
+// #[global_allocator]
+#[allow(dead_code)]
 static DUMMY_ALLOCATOR: Dummy = Dummy;
 
 #[alloc_error_handler]
 fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
   panic!("allocation error: {:?}", layout)
 }
+
+/// Start address of heap virtual memory
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+/// Heap size
+pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+
+/// Initialize kernel's heap memory area by mapping all pages
+/// in kernel's [HEAP_START, HEAP_START + HEAP_SIZE] range to
+/// physical frames.
+pub fn init_heap(
+  mapper: &mut impl Mapper<Size4KiB>,
+  frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) -> Result<(), MapToError<Size4KiB>> {
+  let page_range = {
+    let heap_start = VirtAddr::new(HEAP_START as u64);
+    let heap_end = heap_start + HEAP_SIZE - 1u64;
+    // Page::containing_address will do the 4KiB alignment for us
+    let heap_start_page = Page::containing_address(heap_start);
+    let heap_end_page = Page::containing_address(heap_end);
+    Page::range_inclusive(heap_start_page, heap_end_page)
+  };
+
+  for page in page_range {
+    let frame = frame_allocator
+      .allocate_frame()
+      .ok_or(MapToError::FrameAllocationFailed)?;
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+    unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
+  }
+
+  // give the initialized memory to allocator
+  unsafe {
+    ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+  }
+
+  Ok(())
+}
+
+/// ALERT: don't use allocation inside an interrupt handler, as that might
+/// cause deadlock for concurrent access to ALLOCATOR
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
